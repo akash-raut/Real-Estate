1fda86ea3c53991b4fe59261d440c1573c52a394
Akash <rautakash1027@gmail.com>
2020-09-23 11:47:46 +0530

    Listings page URL corrected. Humanize added for better look of pricing & for how old is property. Paginator added and listings query managed to show only active listings and sorted descending order.

--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 
+from .models import Listing
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 def index(request):
-    '''  '''
-    return render(request, 'listings/listings.html')
+    ''' Listings of all the Property with little information '''
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    '''  '''
+def listing(request, listing_id):
+    ''' Single listing page for more info about particular Property '''
     return render(request, 'listings/listing.html')
 
 
 def search(request):
-    '''  '''
+    ''' Search with specific requirement in all the listings '''
     return render(request, 'listings/search.html')
 
 
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('listing/', include('listings.urls')),
+    path('listings/', include('listings.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
